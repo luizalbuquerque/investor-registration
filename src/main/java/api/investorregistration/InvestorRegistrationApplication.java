b2bf36ad7a6aec2e01f4499f2a949c1b2b93d53a
Luiz de Albuquerque <luiz.info.2016@gmail.com>
2023-03-15 17:52:43 -0300

    first commit

---
+++ src/main/java/api/investorregistration/InvestorRegistrationApplication.java
@@ -0,0 +1,13 @@
+package api.investorregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestorRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/investorregistration/InvestorRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package api.investorregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestorRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
